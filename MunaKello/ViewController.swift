be3425f78e411f547e34dbd66b6f9641cd7043ea
Juuso Loikkanen <Juuso@macbook-pro.local>
2022-12-04 23:19:38 +0200

    Initial Commit

---
+++ MunaKello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MunaKello
+//
+//  Created by Juuso Loikkanen on 4.12.2022.
+//  Copyright © 2022 Simo Loikkanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
