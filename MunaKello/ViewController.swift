f3c840aa7d9b324f014f70f9171354ab58086b7d
Juuso Loikkanen <unpocoloco064@gmail.com>
2023-01-14 12:57:23 +0200

    Basic Timer

--- MunaKello/ViewController.swift
+++ MunaKello/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
+var player: AVAudioPlayer?
+
+// MARK: - Outlets
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // MARK: - Dictionary
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totaltime = 0
+    var secondsPassed = 0
+    
+   
+    
+    // MARK: - Actions
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totaltime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        //MARK: - Timer mechanism
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self,
+        // From objective C
+            selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        
+        if secondsPassed < totaltime {
+            
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totaltime)
+            let percentageProgress = secondsPassed / totaltime
+            print(percentageProgress)
+            print(Float(secondsPassed) / Float(totaltime))
+            
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            //MARK: - Play sound, important
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player?.play()
+        }
+      
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
+
 
